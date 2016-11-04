2f196d7b1af6d19806435f0333e17f0c3f3549bb
bryan valdez <ibryan.valdez@gmail.com>
2016-11-04 17:51:35 -0500

    cs

--- js/alumnoCtrl.js
+++ js/alumnoCtrl.js
@@ -1,5 +1,17 @@
-app.controller('alumnoCtrl', ['$scope', function ($scope) {
+app.controller('alumnoCtrl', ['$scope', '$routeParams', '$http',  function ($scope, $routeParams, $http) {
 	
-	$scope.mensaje = "hola";
+	$scope.setActive("mAlumnos");
+	var codigo = $routeParams.codigo;
+
+	$scope.alumno = {};
+
+	$http.get('php/servicios/alumnos.getAlumno.php?c=' + codigo).success(function(data){
+		if(data.err !== undefined){
+			window.location = '#/alumnos';
+			return;
+		}
+		$scope.alumno = data;
+	});
 	
 }]);
+
